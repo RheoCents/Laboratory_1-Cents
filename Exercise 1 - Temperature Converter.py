54db3b497807d82ed0440e29a1d83416c91e66ed
unknown <centinaraphael0157@gmail.com.>
2024-09-19 22:11:17 +0800

    minor error handling and adjustments

--- Exercise 1 - Temperature Converter.py
+++ Exercise 1 - Temperature Converter.py
@@ -2,20 +2,24 @@ print("Welcome to Rheo's temperature converter\nthis converts Fahrenheit to Cels
 def temperature_Converter():
     try:
         user_temperature_value = input("Enter the temperature that you want to convert. e.g. 34\n")
-        user_temperature_type = input("""Enter your corresponding conversion type (CTF or FTC)
+        if user_temperature_value.isalpha():
+            print("please only enter intigers")
+            temperature_Converter()
+        else:
+            user_temperature_type = input("""Enter your corresponding conversion type (CTF or FTC)
 from Celsius to Fahrenheit - CTF
 from Fahrenheit to Celsius - FTC
 """ )
-        temperture_value = float(user_temperature_value)
-        if user_temperature_type.upper() == "CTF":
-            result = (temperture_value*1.8)+32
-            print(f"The resulting temperature is {result}°F")
-        elif user_temperature_type.upper() == "FTC":
-            result = (temperture_value - 32)/1.8
-            print(f"The resulting temperature is {result}°C")
-        else:
-            print("Please select between FTC or CTF")
-            temperature_Converter()
+            temperture_value = float(user_temperature_value)
+            if user_temperature_type.upper() == "CTF":
+                result = (temperture_value*1.8)+32
+                print(f"The resulting temperature is {result}°F")
+            elif user_temperature_type.upper() == "FTC":
+                result = (temperture_value - 32)/1.8
+                print(f"The resulting temperature is {result}°C")
+            else:
+                print("Please select between FTC or CTF")
+                temperature_Converter()
     except TypeError:
         print("Please follow the needed format")
     except  ValueError:
--- Exercise 3 - Diamond Shape (advance topic).py
+++ Exercise 3 - Diamond Shape (advance topic).py
@@ -1,7 +1,6 @@
 print("This is Rheo's Diamond printer\nYou will enter an odd intiger and it will print a diamond")
 def diamond_printer():
     try:
-
         user_odd_integer = int(input("Please enter an odd integer: "))
         if user_odd_integer % 2 != 0:  
             for i in range(user_odd_integer // 2 + 1):
